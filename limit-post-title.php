21b79f178e03fa437fd3560e43c5a8f0f6eb0343
Arklog <pierr.ducloux@gmail.com>
2021-04-26 11:37:38 +0200

    removed testing lines

--- limit-post-title.php
+++ limit-post-title.php
@@ -6,18 +6,14 @@
  */
 
 
- function lpt_filter_title($title) {
-     
+function lpt_filter_title($title)
+{
+
     $len = 33; //the maximun number of characters to display
     $trail = "..."; //trailing characters
 
-    if(strlen($title) > $len && !is_single(  ))
+    if (strlen($title) > $len && !is_single())
         return substr($title, 0, $len) . $trail;
     return $title;
- }
-//add_filter('the_title', 'lpt_filter_title', 10, 1 );
-
-function lpt_shortcode() {
-	return "<h2>test</h2>";
 }
-add_shortcode("my-shortcode", lpt_shortcode);
+add_filter('the_title', 'lpt_filter_title', 10, 1);
